7d077f976649d7d0d9e85af6dc19f5d380cfecf2
Suraj Kumar <sukr7581@gmail.com>
2025-08-13 22:41:13 +0530

    Added Answers model

--- src/main/java/com/example/QuoraApp/models/Answers.java
+++ src/main/java/com/example/QuoraApp/models/Answers.java
@@ -1,13 +1,39 @@
 package com.example.QuoraApp.models;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "answers")
 public class Answers {
 
     @Id
     private String id;
 
-    @NotBlank
+    @NotBlank(message = "Content is required")
+    @Size(min = 10, max = 1000, message="Content must be between 10 and 1000 characters")
     private String content;
+
+    @Indexed
+    private String questionId;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }
