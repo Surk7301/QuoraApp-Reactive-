1dcb9ba2af1cd72b76f5a29f8f2f1af21d88fc66
Suraj Kumar <sukr7581@gmail.com>
2025-08-21 23:05:46 +0530

    Implemented consumer factory

--- src/main/java/com/example/QuoraApp/config/KafkaConfig.java
+++ src/main/java/com/example/QuoraApp/config/KafkaConfig.java
@@ -1,12 +1,17 @@
 package com.example.QuoraApp.config;
 
+import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonSerializer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.ProducerFactory;
 
@@ -32,5 +37,16 @@ public class KafkaConfig {
 
         return new DefaultKafkaProducerFactory<>(configProps);
 
+    }
+
+    @Bean
+    public ConsumerFactory<String, Object> consumerFactory(){
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        configProps.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+
+        return new DefaultKafkaConsumerFactory<>(configProps);
     }
 }
