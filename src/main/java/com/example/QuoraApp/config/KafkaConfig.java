e09fc1d993e1d489311e4e492e04c6c2624b9f5c
Suraj Kumar <sukr7581@gmail.com>
2025-08-21 23:42:49 +0530

    Configured kafka

--- src/main/java/com/example/QuoraApp/config/KafkaConfig.java
+++ src/main/java/com/example/QuoraApp/config/KafkaConfig.java
@@ -10,10 +10,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -27,6 +25,8 @@ public class KafkaConfig {
 
     @Value("${spring.kafka.consumer.group-id:view-count-consumer}")
     private String groupId;
+
+    public static final String TOPIC_NAME = "view-count-topic";
 
     @Bean
     public ProducerFactory<String, Object> producerFactory(){
@@ -48,5 +48,18 @@ public class KafkaConfig {
         configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
 
         return new DefaultKafkaConsumerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate(){
+        return  new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(){
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        factory.setConcurrency(3);
+        return factory;
     }
 }
